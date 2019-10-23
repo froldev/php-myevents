b9493bd3143df24d19f10673d799c6dbad7cc947
Aurélien Chaillot <chaillot.aurelien@gmail.com>
2019-10-23 17:32:40 +0200

    add_event_1

---
+++ src/Controller/EventsController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class EventsController extends AbstractController
+
+{
+    public function add(): string
+    {
+        return $this->twig->render("Events/add.html.twig");
+    }
+}
