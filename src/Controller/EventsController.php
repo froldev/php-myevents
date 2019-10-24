2df16affe6edd0ea6daa44aa56c39f39c1813e24
Aurélien Chaillot <chaillot.aurelien@gmail.com>
2019-10-24 16:19:17 +0200

    add event error messages

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -2,10 +2,41 @@
 
 namespace App\Controller;
 
+use App\Model\EventsManager;
+
 class EventsController extends AbstractController
 {
-    public function add(): string
+    public function add()
     {
-        return $this->twig->render("Events/add.html.twig");
+        $nameError = $dateError = $descriptionError = $priceError = null;
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $isValid = true;
+            if (empty($_POST["name"]) || !isset($_POST["name"])) {
+                $nameError = "Indiquez un nom d'évènement";
+                $isValid = false;
+            }
+            if (empty($_POST["date_event"]) || !isset($_POST["date_event"])) {
+                $dateError = "Indiquez une date";
+                $isValid = false;
+            }
+            if (empty($_POST["description"]) || !isset($_POST["description"])) {
+                $descriptionError = "Décrivez l'évènement";
+                $isValid = false;
+            }
+            if (empty($_POST["price"]) || !isset($_POST["price"])) {
+                $priceError = "Indiquez un prix";
+                $isValid = false;
+            }
+            if ($isValid) {
+                $eventsManager = new EventsManager();
+                $eventsManager->insertEvent($_POST);
+            }
+        }
+        return $this->twig->render("Events/add.html.twig", [
+            "nameError" => $nameError,
+            "dateError" => $nameError,
+            "descriptionError" => $nameError,
+            "priceError" => $nameError,
+        ]);
     }
 }
--- src/Model/EventsManager.php
+++ src/Model/EventsManager.php
@@ -4,5 +4,23 @@ namespace App\Model;
 
 class EventsManager extends AbstractManager
 {
+    const TABLE = "events";
 
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    public function insertEvent(array $event): bool
+    {
+        $request = $this->pdo->prepare("INSERT INTO".self::table."(name, date, description, price) VALUES 
+        (:name, :date_event, :description, :price)");
+
+        $request->bindValue(":name", $event["name"], \PDO::PARAM_STR);
+        $request->bindValue(":date", $event["date"], \PDO::PARAM_STR);
+        $request->bindValue(":description", $event["description"], \PDO::PARAM_STR);
+        $request->bindValue(":price", $event["price"], \PDO::PARAM_STR);
+
+        $request->execute();
+    }
 }
