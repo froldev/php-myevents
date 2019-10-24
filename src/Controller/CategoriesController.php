dd00ed8c63e92bfcdc9846fd13ffe5b8bb88b647
Fred OLIVE <frol.developpement@gmail.com>
2019-10-24 16:37:55 +0200

    create add category

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -15,4 +15,26 @@ class CategoriesController extends AbstractController
                 "categories" => $categories,
             ]);
     }
+
+    public function add()
+    {
+        $categoryError = null;
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $isValid = true;
+            if (empty($_POST['category']) || !isset($_POST['category'])) {
+                $categoryError = "Merci de saisir une nouvelle catégorie";
+                $isValid = false;
+            }
+            if ($isValid) {
+                $categoriesManager = new CategoriesManager();
+                if ($categoriesManager->insertCategories($_POST)) {
+                    header("Location:/categories/list");
+                }
+            }
+        }
+
+        return $this->twig->render("Categories/add.html.twig", [
+            "categoryError" => $categoryError,
+        ]);
+    }
 }
