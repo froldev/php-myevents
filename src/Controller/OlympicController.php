278410c1ce51f6cb71f850916af4241be6f8485a
Aurélien Chaillot <chaillot.aurelien@gmail.com>
2019-11-08 14:53:23 +0100

    update road

--- src/Controller/OlympicController.php
+++ src/Controller/OlympicController.php
@@ -6,7 +6,7 @@ use App\Model\AbstractManager;
 
 class OlympicController extends AbstractController
 {
-    public function info()
+    public function information()
     {
         return $this->twig->render('Olympic/info.html.twig');
     }
