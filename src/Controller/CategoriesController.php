691f0b499673ee7eaef14f5d4e3deb28830f3ca7
Benjamin Désigné <designebenjamin@gmail.com>
2019-11-13 09:52:44 +0100

    resolve bug from search to details events

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -15,7 +15,6 @@ class CategoriesController extends AbstractController
         return $this->twig->render("Categories/list.html.twig", [
                 "categories" => $categories,
             ]);
-
     }
 
     public function add(): string
@@ -61,9 +60,6 @@ class CategoriesController extends AbstractController
                 header("Location:/categories/list");
             }
         }
-
-
         return $this->twig->render('Categories/edit.html.twig', ['category' => $category]);
-
     }
 }
--- src/Model/EventsManager.php
+++ src/Model/EventsManager.php
@@ -14,21 +14,6 @@ class EventsManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-    public function insertEvent(array $event): bool
-    {
-        $request = $this->pdo->prepare("INSERT INTO ".self::TABLE." (title, date_time, description, price, 
-        image) VALUES 
-        (:title, :date_time, :description, :price, :image)");
-
-        $request->bindValue(":title", $event["title"], \PDO::PARAM_STR);
-        $request->bindValue(":date_time", $event["date_time"], \PDO::PARAM_STR);
-        $request->bindValue(":description", $event["description"], \PDO::PARAM_STR);
-        $request->bindValue(":price", $event["price"], \PDO::PARAM_INT);
-        $request->bindValue(":image", $event["image"], \PDO::PARAM_INT);
-
-        return $request->execute();
-    }
-
     public function deleteEvent(int $id): void
     {
         $request = $this->pdo->prepare("DELETE FROM ".self::TABLE." WHERE id=:id");
