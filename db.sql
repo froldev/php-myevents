db4d4d1fa33e8dcfcb1a0d57f993b7778e38fe9e
Fred OLIVE <frol.developpement@gmail.com>
2020-12-29 10:24:56 +0100

    modify page informations

--- db.sql
+++ db.sql
@@ -39,10 +39,11 @@ CREATE TABLE society (
     phone VARCHAR(20) NOT NULL,
     twitter VARCHAR(100) NOT NULL,
     facebook VARCHAR(100) NOT NULL,
-    instagram VARCHAR(100) NOT NULL
+    instagram VARCHAR(100) NOT NULL,
+    google_map TEXT NULL
 );
 
-INSERT INTO society (name, picture, address, cp, town, mail, phone, twitter, facebook, instagram)
+INSERT INTO society (name, picture, address, cp, town, mail, phone, twitter, facebook, instagram, google_map)
 VALUES
 ("OLYMPIC NANTAIS",
 "https://zupimages.net/up/20/52/xa4o.png",
@@ -53,8 +54,9 @@ VALUES
 "99.99.99.99.99",
 "https://twitter.com/Olympic-Nantais",
 "https://www.facebook.com/Olympic-Nantais",
-"https://www.instagram.com/Olympic-Nantais")
-;
+"https://www.instagram.com/Olympic-Nantais",
+"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1355.2421153034522!2d-1.556686641767044!3d47.207106794790064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805eeab942799ed%3A0x3c5a7b2e9e2b56ec!2s2%20Quai%20Fran%C3%A7ois%20Mitterrand%2C%2044200%20Nantes!5e0!3m2!1sfr!2sfr!4v1609188196751!5m2!1sfr!2sfr"
+);
 
 CREATE TABLE navbar (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
